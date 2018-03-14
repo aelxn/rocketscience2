8aee909357090a10a4ae8baabebcb8cdb869e384
aelxn <37371365+aelxn@users.noreply.github.com>
2018-03-14 10:34:32 -0400

    Updated from Brython Server: 3/14/2018 10:34:06 AM

--- rocket2.py
+++ rocket2.py
@@ -1,6 +1,20 @@
 from ggrocket import Rocket, Planet
-from math import radians
+from math import radians, sqrt
+from ggmath import Slider
+# Add a slider for controlling the timezoom
+tz = Slider((10,400), 0, 5, 0, positioning="physical")
 
 earth = Planet()
-rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=10)
+
+# Constants relating to Earth and physics
+Re = 6.371E6  # Earth radius: 6371000 meters in scientific notation
+Me = 5.972E24 # Earth mass in kg (5.972 x 10^24)
+G = 6.674E-11 # Gravitational constant
+
+# Calculate the escape velocity from Earth's surface radius
+Ve=sqrt(2*Me*G/Re)
+print("Predicted escape velocity is ", Ve, " m/s")
+rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve, timezoom=tz)
+
+rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve)
 earth.run(rocket)
